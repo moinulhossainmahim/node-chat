70aa11d4b35789b351e69e3ec7c62a8c8f1891f5
mhossain2 <moinulhossainmahim@gmail.com>
2024-06-06 19:10:03 +0600

    revert index

--- api/index.js
+++ api/index.js
@@ -1,10 +1,32 @@
-const express = require('express');
-const app = express();
 const http = require('http');
-const socket = require('../socket');
+const socketIo = require('socket.io');
+const chatModel = require('../models/chat');
+const app = require('../app');
 
 const server = http.createServer(app);
-socket.listen(server);
+const io = socketIo(server);
+
+io.on('connection', socket => {
+  console.log(`Client (${socket.id}) is connected.`);
+
+  socket.on('typing', data => {
+    socket.broadcast.emit('typing', data);
+  });
+
+  socket.on('message', data => {
+    chatModel.create(data)
+      .then(chat => {
+        chat.populate('user')
+          .then(chat => {
+            io.sockets.emit('message', chat);
+          });
+      });
+  });
+
+  socket.on('disconnect', reason => {
+    console.log(`Client (${socket.id}) is disconnected.`);
+  });
+});
 
 module.exports = (req, res) => {
   server.emit('request', req, res);
