46fb929897c1baff51b744c498ef03a1eaccb850
mhossain2 <moinulhossainmahim@gmail.com>
2024-06-06 17:39:37 +0600

    updated mongoose

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    'mongodb+srv://nodejs-chat:nodejs-chat@nodejs-chat.khttal0.mongodb.net/?retryWrites=true&w=majority&appName=nodejs-chat'
-);
+mongoose.connect('mongodb+srv://nodejs-chat:nodejs-chat@nodejs-chat.khttal0.mongodb.net/?retryWrites=true&w=majority&appName=nodejs-chat');
